8c4fb2f64f8b0ce713b023e9ee1cd5b7fc2a6db2
unknown <lathagowda009@gmail.com>
2022-10-13 21:44:15 +0530

    object and program

---
+++ 11-10-2022/Latha.java
@@ -0,0 +1,14 @@
+class Latha{
+	String cityName;
+	static String clgName="jvit";
+	public static void main(String[] args){
+	int i=30;
+	System.out.println(i);
+	Latha l=new Latha();
+	l.cityName="bidadi";
+
+	System.out.println(l.cityName);
+
+	System.out.println(clgName);
+	}
+}
---
+++ 11-10-2022/Variables.java
@@ -0,0 +1,14 @@
+class Variables{
+	String name;
+	static String brotherName="AnujithGowda";
+	public static void main(String[] args){
+	int i=10;
+	System.out.println(i);
+	Variables v=new Variables();
+	v.name="latha";
+
+	System.out.println(v.name);
+
+	System.out.println(brotherName);
+	}
+}
