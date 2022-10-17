a5307c2d74efd3b1ae48e8627bb5de1a8fc182dd
unknown <lathagowda009@gmail.com>
2022-10-17 20:42:07 +0530

    control flow statement

---
+++ Features/nestedif/17-10-2022/Manoj.java
@@ -0,0 +1,20 @@
+class Latha{
+	int age;
+	int weight;
+	public static void main(String[]args){
+	Latha l=new Latha();
+	l.age=18;
+	l.weight=40;
+	if(l.age<=20){
+		if(l.weight<=50){
+		
+		System.out.println("You are eligible to donate blood");
+		
+		}
+	
+	}
+
+
+	}
+
+}
